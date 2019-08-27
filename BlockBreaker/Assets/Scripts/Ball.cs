b3d11296dd5b13b3acb60fd729d40cf2a6e15e3e
Carter Ithier <ithier@Carters-MacBook-Pro.local>
2019-08-27 09:19:24 -0400

    added different breakability levels of blocks

--- BlockBreaker/Assets/Scripts/Ball.cs
+++ BlockBreaker/Assets/Scripts/Ball.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,6 +9,7 @@ public class Ball : MonoBehaviour
     [SerializeField] float xVeloc = 2f;
     [SerializeField] float yVeloc = 15f;
     [SerializeField] AudioClip[] ballSounds;
+    [SerializeField] float randomFactor = .5f;
 
     // state
     Vector3 paddleToBallVector;
@@ -17,12 +17,14 @@ public class Ball : MonoBehaviour
 
     // cached componenet references
     AudioSource myAudioSource;
+    Rigidbody2D myRigidBody2D;
 
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
         myAudioSource = GetComponent<AudioSource>();
+        myRigidBody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -42,7 +44,7 @@ public class Ball : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             hasStarted = true;
-            GetComponent<Rigidbody2D>().velocity = new Vector2(xVeloc, yVeloc);
+            myRigidBody2D.velocity = new Vector2(xVeloc, yVeloc);
         }
     }
 
@@ -56,10 +58,14 @@ public class Ball : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Vector2 velocityTweak = new Vector2(Random.Range(0f, randomFactor),
+            Random.Range(0f,randomFactor));
+
         if (hasStarted)
         {
-            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            AudioClip clip = ballSounds[Random.Range(0, ballSounds.Length)];
             myAudioSource.PlayOneShot(clip);
+            myRigidBody2D.velocity += velocityTweak;
         }
         
     }
--- BlockBreaker/Assets/Scripts/Block.cs
+++ BlockBreaker/Assets/Scripts/Block.cs
@@ -1,15 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    // config params
     [SerializeField] AudioClip breakBlock;
     [SerializeField] GameObject blockSparklesVFX;
+    [SerializeField] Sprite[] hitSprites;
+    
 
     // cached reference
     Level level;
     GameStatus gameStatus;
+
+    // state variables
+    [SerializeField] int timesHit; // TODO only serialized for debug purposes
 
     private void Start()
     {
@@ -30,13 +37,40 @@ public class Block : MonoBehaviour
     {
         if (tag == "Breakable")
         {
-            DestroyBlock();
+            HandleHit();
         }
         else
         {
             PlayBlockDestroySFX();
         }
         
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        int maxHits = hitSprites.Length + 1;
+        if (timesHit >= maxHits)
+        {
+            DestroyBlock();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        if (hitSprites[spriteIndex] != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        else
+        {
+            Debug.LogError("Block sprite is missing from array for " + gameObject.name);
+        }
     }
 
     private void DestroyBlock()
